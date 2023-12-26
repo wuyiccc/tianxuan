55e363e608734991a77c0c95de6395f029a3594b
wuyiccc <3406324191@qq.com>
2023-12-26 22:53:47 +0800

    feat: 客户端管理

--- tianxuan-chat/src/main/java/com/wuyiccc/chat/websocket/handler/ChatHandler.java
+++ tianxuan-chat/src/main/java/com/wuyiccc/chat/websocket/handler/ChatHandler.java
@@ -4,7 +4,10 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelId;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 /**
  * @author wuyiccc
@@ -13,6 +16,9 @@ import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
  */
 public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
 
+
+    // 用于记录和管理所有客户端的channel
+    public static ChannelGroup clients = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame msg) throws Exception {
@@ -31,6 +37,45 @@ public class ChatHandler extends SimpleChannelInboundHandler<TextWebSocketFrame>
         System.out.println("客户端currentChannelIdShort: " + currentChannelIdShort);
 
         TextWebSocketFrame replayMsg = new TextWebSocketFrame("当前客户端的id为: " + currentChannelIdShort);
-        currentChannel.writeAndFlush(replayMsg);
+
+        // 测试向所有客户端群发消息
+        //for (Channel channel : clients) {
+        //    channel.writeAndFlush(replayMsg);
+        //}
+        clients.writeAndFlush(replayMsg);
+
+
+    }
+
+    // 客户端连接到服务端之后触发, 打开链接
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+
+        String channelId = ctx.channel().id().asLongText();
+        System.out.println("客户端连接, channel对应的长id为: " + channelId);
+
+        // 获取客户端的channel, 并放入到channelGroup中进行管理
+        clients.add(ctx.channel());
+    }
+
+    // 客户端关闭连接
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        // 当触发handlerRemoved, channelGroup自动移除对应客户端的channel
+        String channelId = ctx.channel().id().asLongText();
+        System.out.println("客户端断开, channel对应的长id为: " + channelId);
+        clients.remove(ctx.channel());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+        cause.printStackTrace();;
+
+        // 发生异常之后, 关闭连接channel
+        ctx.channel().close();
+        // 从channelGroup中移除对应的channel
+        clients.remove(ctx.channel());
+
     }
 }
