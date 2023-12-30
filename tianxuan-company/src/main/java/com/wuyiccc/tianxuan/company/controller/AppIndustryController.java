598c3d82347a1776d0444f522ffb86b5a28ed5e5
wuyiccc <3406324191@qq.com>
2023-12-31 07:56:08 +0800

    feat: redis缓存

--- tianxuan-company/src/main/java/com/wuyiccc/tianxuan/company/controller/AppIndustryController.java
+++ tianxuan-company/src/main/java/com/wuyiccc/tianxuan/company/controller/AppIndustryController.java
@@ -1,12 +1,19 @@
 package com.wuyiccc.tianxuan.company.controller;
 
+import cn.hutool.json.JSONUtil;
+import com.wuyiccc.tianxuan.common.base.BaseInfoProperties;
 import com.wuyiccc.tianxuan.common.result.CommonResult;
+import com.wuyiccc.tianxuan.common.util.RedisUtils;
 import com.wuyiccc.tianxuan.company.service.IndustryService;
 import com.wuyiccc.tianxuan.pojo.Industry;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author wuyiccc
@@ -20,10 +27,24 @@ public class AppIndustryController {
     @Resource
     private IndustryService industryService;
 
+    @Resource
+    private RedisUtils redisUtils;
+
 
     @GetMapping("/getTopList")
     public CommonResult<List<Industry>> getTopIndustryList() {
-        List<Industry> resList = industryService.getTopIndustryList();
+
+        // 先从redis中查询, 如果没有, 再从db中查询后并放入redis中
+        String topIndustryListStr = redisUtils.get(BaseInfoProperties.TOP_INDUSTRY_LIST);
+
+        List<Industry> resList = null;
+
+        if (Objects.nonNull(topIndustryListStr)) {
+            resList = JSONUtil.toList(topIndustryListStr, Industry.class);
+        } else {
+            resList = industryService.getTopIndustryList();
+            redisUtils.set(BaseInfoProperties.TOP_INDUSTRY_LIST, JSONUtil.toJsonStr(resList));
+        }
         return CommonResult.ok(resList);
     }
 
@@ -31,7 +52,16 @@ public class AppIndustryController {
     @GetMapping("/getThirdListByTop/{topIndustryId}")
     public CommonResult<List<Industry>> getThirdIndustryListByTop(@PathVariable("topIndustryId") String topIndustryId) {
 
-        List<Industry> resList = industryService.getThirdIndustryListByTop(topIndustryId);
+        String resListStr = redisUtils.get(BaseInfoProperties.THIRD_INDUSTRY_LIST + ":byTopId:" + topIndustryId);
+
+        List<Industry> resList;
+        if (Objects.nonNull(resListStr)) {
+            resList = JSONUtil.toList(resListStr, Industry.class);
+        } else {
+            resList = industryService.getThirdIndustryListByTop(topIndustryId);
+
+            redisUtils.set(BaseInfoProperties.THIRD_INDUSTRY_LIST + ":byTopId:" + topIndustryId, JSONUtil.toJsonStr(resList));
+        }
 
         return CommonResult.ok(resList);
     }
