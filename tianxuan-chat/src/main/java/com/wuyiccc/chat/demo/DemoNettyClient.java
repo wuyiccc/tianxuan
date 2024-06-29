7f4344bf7f392ec3809320913cd2f863286373c1
wuyiccc <3406324191@qq.com>
2024-06-29 10:05:14 +0800

    feat: netty客户端链接失败进行重连

--- tianxuan-chat/src/main/java/com/wuyiccc/chat/demo/DemoNettyClient.java
+++ tianxuan-chat/src/main/java/com/wuyiccc/chat/demo/DemoNettyClient.java
@@ -2,12 +2,15 @@ package com.wuyiccc.chat.demo;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wuyiccc
@@ -15,12 +18,19 @@ import java.util.Date;
  */
 public class DemoNettyClient {
 
+
+    private static final Integer MAX_RETRY = 5;
+
     public static void main(String[] args) throws InterruptedException {
         Bootstrap bootstrap = new Bootstrap();
         NioEventLoopGroup group = new NioEventLoopGroup();
 
-        bootstrap.group(group)
+        bootstrap
+                // 指定线程模型
+                .group(group)
+                // 指定IO模型为NIO
                 .channel(NioSocketChannel.class)
+                // IO处理逻辑
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) {
@@ -28,11 +38,64 @@ public class DemoNettyClient {
                     }
                 });
 
-        Channel channel = bootstrap.connect("127.0.0.1", 8000).channel();
+        // 建立连接
+        //Channel channel = bootstrap.connect("127.0.0.1", 8000)
+        //        .addListener(new ChannelFutureListener() {
+        //            @Override
+        //            public void operationComplete(ChannelFuture future) throws Exception {
+        //                if (future.isSuccess()) {
+        //                    System.out.println("连接建立成功");
+        //                } else {
+        //                    System.out.println("连接建立失败");
+        //                }
+        //            }
+        //        })
+        //        .channel();
 
-        while (true) {
-            channel.writeAndFlush(new Date() + ": hello world!");
-            Thread.sleep(2000);
-        }
+        connect(bootstrap, "127.0.0.1", 9000, MAX_RETRY);
+
+        //while (true) {
+        //    channel.writeAndFlush(new Date() + ": hello world!");
+        //    Thread.sleep(2000);
+        //}
+    }
+
+
+    private static void connect(Bootstrap bootstrap, String host, int port) {
+
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功");
+            } else {
+                System.out.println("连接失败, 开始重连");
+                connect(bootstrap, host, port);
+            }
+        });
+    }
+
+
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+
+
+        bootstrap.connect(host, port).addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                if (future.isSuccess()) {
+                    System.out.println("链接成功");
+                } else if (retry == 0) {
+                    System.out.println("重试次数已用完, 放弃连接");
+                } else {
+
+                    // 第几次重连
+                    int order = (MAX_RETRY - retry) + 1;
+                    int delay = 1 << order;
+                    System.err.println(new Date() + ": 链接失败, 第" + order + "次重连......");
+                    // 定时间隔重连
+                    bootstrap.config().group().schedule(() -> connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
+                }
+            }
+        });
     }
 }
+
+
